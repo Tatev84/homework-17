822097de874880970b2a03995a0483048bf16511
Tatev84 <tatevh84@gmail.com>
2021-08-11 08:51:43 +0400

    homework 17

---
+++ script.js
@@ -0,0 +1,46 @@
+ /* function upper(Morning){
+    let reg=/[^A-Z]/
+    if(reg.test(Morning)){
+        return true
+
+        }else{
+            return false
+        }
+    }
+
+
+console.log(upper())
+ */
+
+/*  let items = ['goal', 'new', 'user', 'sit', 'eat', 'dinner']
+let reg=items.filter(word=>/word[^e]/g) */
+   
+
+
+
+
+
+
+/* let ip = 'They ate 5 apples and 5 oranges'
+let reg=/5/
+console.log(ip.replace(reg,"five")) */
+
+
+/* let items = ['lovely', 'fgdentist', '2 lonely', 'eden', 'flyn', 'dent']
+let reg=items.filter(word=>/word[^den.*ly$]/g) 
+console.log(items.match(reg))
+ */
+
+ 
+/* let str=" Grigor ` (077)00-01-02, Mariam `(098)02-01-00,Armen`(077)01-02-00";
+let reg=/^([0-9]{3})$-[0-9]{2}-[0-9]{2}-[0-9]{2}/g
+console.log(str.match(reg)) */
+
+
+
+
+
+
+/* let str="www,site,com   www,bigsite,org  www,supersite,net";
+let reg=/,/g
+console.log(str.replace(reg,"."))  */
